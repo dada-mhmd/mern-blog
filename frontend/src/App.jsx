d6774db49dc6a7c32477af5804ee30bfa9a7a085
dada-mhmd <moudi.ld@hotmail.com>
2024-01-20 21:31:14 -0800

    created navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,13 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import Project from './pages/Project';
+import Header from './components/Header';
 
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
