b526fa6e2f3bae0aa974dd7c8163e600f0d2b582
dada-mhmd <moudi.ld@hotmail.com>
2024-01-20 22:49:48 -0800

    connect to database

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
 import express from 'express'
+import 'dotenv/config.js'
+
+import connectDB from './config/database.js';
 
 
 const port  = process.env.PORT || 5000
 const app = express()
+
+connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
