0d46de68ab15191efb28bf7d68a619b0ab3933a9
dada-mhmd <moudi.ld@hotmail.com>
2024-01-21 22:31:27 -0800

    make the register page functional and added the footer

--- backend/controllers/authCtrl.js
+++ backend/controllers/authCtrl.js
@@ -1,26 +1,31 @@
-import bcrypt from 'bcryptjs'
-import User from "../models/userModel.js"
+import bcrypt from 'bcryptjs';
+import User from '../models/userModel.js';
 import asyncHandler from './../middleware/asyncHandler.js';
 
 export const signup = asyncHandler(async (req, res) => {
-    const { username, email, password } = req.body
+  const { username, email, password } = req.body;
 
-    if(!username || !email || !password || username === '' || email === '' || password === '') {
-        res.status(400)
-        throw new Error('Please add all fields')
-    }
-    const user = await User.findOne({email})
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === '' ||
+    email === '' ||
+    password === ''
+  ) {
+    res.status(400);
+    throw new Error('Please add all fields');
+  }
+  const user = await User.findOne({ email });
 
-    if(user) {
-        res.status(400)
-        throw new Error('User already exists')
-    }
-    
-    const hashedPassword = await bcrypt.hash(password, 10)
+  if (user) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
 
-    await User.create({username, email, password:hashedPassword})
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    res.send('Signup Successful')
+  await User.create({ username, email, password: hashedPassword });
 
-}
-)
+  res.json({ message: 'User created successfully' });
+});
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,9 @@
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+
+import 'react-toastify/dist/ReactToastify.css';
+
+
 import Home from './pages/Home';
 import About from './pages/About';
 import Login from './pages/Login';
@@ -6,20 +11,26 @@ import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import Project from './pages/Project';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 
 const App = () => {
   return (
     <BrowserRouter>
     <Header />
+      <main className='min-h-screen'>
+      <ToastContainer />
       <Routes>
+
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
-        <Route path='/sign-in' element={<Login />} />
+        <Route path='/login' element={<Login />} />
         <Route path='/sign-up' element={<Register />} />
         <Route path='/dashboard' element={<Dashboard />} />
         <Route path='/projects' element={<Project />} />
       </Routes>
+      </main>
+      <Footer />
     </BrowserRouter>
   )
 }
