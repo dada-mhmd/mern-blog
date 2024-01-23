f22d2270800d7631d2eed80ef375ca6aed030794
dada-mhmd <moudi.ld@hotmail.com>
2024-01-22 21:44:24 -0800

    added private route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,7 @@
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
-import { ToastContainer } from 'react-toastify'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 import 'react-toastify/dist/ReactToastify.css';
-
 
 import Home from './pages/Home';
 import About from './pages/About';
@@ -12,27 +11,30 @@ import Dashboard from './pages/Dashboard';
 import Project from './pages/Project';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
+import PrivateRoute from './components/PrivateRoute';
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <main className='min-h-screen'>
-      <ToastContainer />
-      <Routes>
+        <ToastContainer />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/sign-up' element={<Register />} />
 
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/sign-up' element={<Register />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/projects' element={<Project />} />
-      </Routes>
+          <Route element={<PrivateRoute />}>
+            <Route path='/dashboard' element={<Dashboard />} />
+          </Route>
+
+          <Route path='/projects' element={<Project />} />
+        </Routes>
       </main>
       <Footer />
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
