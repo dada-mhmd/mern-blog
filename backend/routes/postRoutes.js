cb42aefa57a95921ce2003b6da03ae953b23735e
dada-mhmd <moudi.ld@hotmail.com>
2024-01-25 22:05:31 -0800

    get posts api and show them on frontend

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { verifyToken } from './../utils/verifyUser.js';
-import { createPost } from '../controllers/postCtrl.js';
+import { createPost, getPosts } from '../controllers/postCtrl.js';
 const router = express.Router();
 
 router.post('/create', verifyToken, createPost);
+router.get('/getPosts', getPosts);
 
 export default router;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,16 +1,14 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
     'node_modules/flowbite-react/lib/esm/**/*.js',
   ],
 
   theme: {
     extend: {},
   },
-  plugins: [
-    // eslint-disable-next-line no-undef
-    require('flowbite/plugin'),
-  ],
-}
+  plugins: [require('flowbite/plugin'), require('tailwind-scrollbar')],
+};
