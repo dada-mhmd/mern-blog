8c2c2a80785a5b8d3e5e0d8e46c949cdda1d9572
dada-mhmd <moudi.ld@hotmail.com>
2024-02-05 13:26:36 -0800

    add projects and about pages

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,33 @@
-
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font-semibold text-center my-7'>
+            About Blog
+          </h1>
+          <div className='text-base text-gray-500 flex flex-col gap-6'>
+            <p>
+              Blog is a blog website where you can learn more about web
+              development, programming languages and share your thoughts.
+            </p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores,
+              vero dignissimos quod, magni, a suscipit blanditiis asperiores id
+              ducimus veritatis nam cumque unde explicabo aut exercitationem
+              sint debitis illo molestiae?
+            </p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Aspernatur explicabo officiis perspiciatis et reiciendis
+              perferendis assumenda veniam, cum sit reprehenderit velit sequi
+              nisi. Veritatis repellat eveniet error! Dolores, in tempora?
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- frontend/src/pages/Project.jsx
+++ frontend/src/pages/Project.jsx
@@ -1,8 +1,14 @@
-
+import CTA from './../components/CTA';
 const Project = () => {
   return (
-    <div>Project</div>
-  )
-}
+    <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
+      <h1 className='text-3xl font-semibold'>Projects</h1>
+      <p className='text-gray-500'>
+        Build fun and interesting projects while learning web development
+      </p>
+      <CTA />
+    </div>
+  );
+};
 
-export default Project
+export default Project;
